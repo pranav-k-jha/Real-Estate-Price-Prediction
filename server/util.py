7ff019701b7cf8c2853a7fa163d7b2ee546995f3
pranav-k-jha <pranav.jha@mail.concordia.ca>
2024-08-10 17:46:03 -0400

    updated server.py

--- server/util.py
+++ server/util.py
@@ -1,16 +1,12 @@
-import os
 import pickle
 import json
 import numpy as np
-
-__base_path = os.getenv("ARTIFACTS_PATH", "/app/artifacts")
 
 __locations = None
 __data_columns = None
 __model = None
 
-
-def get_estimated_price(location, sqft, bhk, bath):
+def get_estimated_price(location,sqft,bhk,bath):
     try:
         loc_index = __data_columns.index(location.lower())
     except:
@@ -20,20 +16,18 @@ def get_estimated_price(location, sqft, bhk, bath):
     x[0] = sqft
     x[1] = bath
     x[2] = bhk
-    if loc_index >= 0:
+    if loc_index>=0:
         x[loc_index] = 1
 
-    return round(__model.predict([x])[0], 2)
+    return round(__model.predict([x])[0],2)
 
 
 def load_saved_artifacts():
     print("loading saved artifacts...start")
-    global __data_columns
+    global  __data_columns
     global __locations
 
-    with open(os.path.join(__base_path,
-                           "columns.json"), "r") as f:
-
+    with open("./artifacts/columns.json", "r") as f:
         __data_columns = json.load(f)['data_columns']
         __locations = __data_columns[3:]  # first 3 columns are sqft, bath, bhk
 
@@ -43,19 +37,16 @@ def load_saved_artifacts():
             __model = pickle.load(f)
     print("loading saved artifacts...done")
 
-
 def get_location_names():
     return __locations
-
 
 def get_data_columns():
     return __data_columns
 
-
 if __name__ == '__main__':
     load_saved_artifacts()
     print(get_location_names())
-    print(get_estimated_price('1st Phase JP Nagar', 1000, 3, 3))
+    print(get_estimated_price('1st Phase JP Nagar',1000, 3, 3))
     print(get_estimated_price('1st Phase JP Nagar', 1000, 2, 2))
-    print(get_estimated_price('Kalhalli', 1000, 2, 2))  # other location
+    print(get_estimated_price('Kalhalli', 1000, 2, 2)) # other location
     print(get_estimated_price('Ejipura', 1000, 2, 2))  # other location
