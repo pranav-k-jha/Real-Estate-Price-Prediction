7117894dc3821bc46518d885b8de57cf45e9539d
pranav-k-jha <pranav.jha@mail.concordia.ca>
2024-08-10 15:39:39 -0400

    updated utils.py

--- server/util.py
+++ server/util.py
@@ -24,24 +24,33 @@ def get_estimated_price(location,sqft,bhk,bath):
 
 def load_saved_artifacts():
     print("loading saved artifacts...start")
-    global  __data_columns
+    global __data_columns
     global __locations
 
-    with open("./artifacts/columns.json", "r") as f:
-        __data_columns = json.load(f)['data_columns']
-        __locations = __data_columns[3:]  # first 3 columns are sqft, bath, bhk
+    try:
+        with open("./artifacts/columns.json", "r") as f:
+            __data_columns = json.load(f)['data_columns']
+            __locations = __data_columns[3:]  # first 3 columns are sqft, bath, bhk
+    except Exception as e:
+        print("Error loading columns.json:", e)
 
     global __model
     if __model is None:
-        with open('./artifacts/banglore_home_prices_model.pickle', 'rb') as f:
-            __model = pickle.load(f)
+        try:
+            with open('./artifacts/banglore_home_prices_model.pickle', 'rb') as f:
+                __model = pickle.load(f)
+        except Exception as e:
+            print("Error loading model:", e)
     print("loading saved artifacts...done")
+
 
 def get_location_names():
     return __locations
 
 def get_data_columns():
     return __data_columns
+
+
 
 if __name__ == '__main__':
     load_saved_artifacts()
